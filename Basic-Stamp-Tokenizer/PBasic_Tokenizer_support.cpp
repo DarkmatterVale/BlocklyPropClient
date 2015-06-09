d7bc92d97aae9f0ea33fda73df3bb1942b222d3b
Vale <valetolpegin@gmail.com>
2015-06-09 10:13:45 -0400

    Added base classes

---
+++ Basic-Stamp-Tokenizer/PBasic_Tokenizer_support.cpp
@@ -0,0 +1,9 @@
+//
+//  PBasic_Tokenizer_support.cpp
+//  
+//
+//  Created by Vale Tolpegin on 4/17/15.
+//
+//
+
+#include "PBasic_Tokenizer_support.h"
---
+++ Basic-Stamp-Tokenizer/PBasic_Tokenizer_support.h
@@ -0,0 +1,14 @@
+//
+//  PBasic_Tokenizer_support.h
+//  
+//
+//  Created by Vale Tolpegin on 4/17/15.
+//
+//
+
+#ifndef ____PBasic_Tokenizer_support__
+#define ____PBasic_Tokenizer_support__
+
+#include <stdio.h>
+
+#endif /* defined(____PBasic_Tokenizer_support__) */
